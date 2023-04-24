508f906e7bb8442184d593e6eb10d10c26dec08f
Анна <124175366+Anna444444@users.noreply.github.com>
2023-04-24 21:36:23 +0600

    ДЗ31

--- ДЗ31/index1.js
+++ ДЗ31/index1.js
@@ -39,6 +39,7 @@ class CoffeeMachine extends StateMachine {
     this.SIG_PREPARE = 'SIG_PREPARE';
     this.SIG_TAKE_DRINK = 'SIG_TAKE_DRINK';
     this.SIG_RETURN_CHANGE = 'SIG_RETURN_CHANGE';
+    this.SIG_RETURN_CHANGE_FINISH = 'SIG_RETURN_CHANGE_FINISH';
     this.paidAmount = 0;
 
     this.sm = [
@@ -48,6 +49,7 @@ class CoffeeMachine extends StateMachine {
       { from: this.ST_PREPARATION, by: this.SIG_PREPARE, to: this.ST_FINISH },
       { from: this.ST_FINISH, by: this.SIG_TAKE_DRINK, to: this.ST_WAIT },
       { from: this.ST_FINISH, by: this.SIG_RETURN_CHANGE, to: this.ST_WAIT },
+      { from: this.ST_FINISH, by: this.SIG_RETURN_CHANGE_FINISH, to: this.ST_FINISH },
     ];
 
     this.currentState = this.ST_WAIT;
@@ -89,99 +91,107 @@ class CoffeeMachine extends StateMachine {
         );
         break;
 
-      case this.ST_PREPARATION:
-        this.allOff();
-        payBtn.style.display = 'none';
-        qrCode.style.display = 'none';
-        coin.style.display = 'none';
-        cup.style.display = 'block';
-        progressBar.style.width = '0%';
-        message.innerText = `Готовлю ${this.selectedDrink.name}`;
-        let progress = 0;
-        let interval = setInterval(() => {
-          progress += 1;
-          progressBar.style.width = `${progress}%`;
-          if (progress === 100) {
-            clearInterval(interval);
-            this.process(this.SIG_PREPARE);
-          }
-        }, 150);
-        break;
-      
-      case this.ST_FINISH:
-        this.allOff();
-        cup.style.display = 'block';
-        message.innerText = 'Возьмите ваш напиток и не забудьте сдачу';
-        changeBtn.removeAttribute('disabled');
-        changeBtn.style.backgroundColor = 'orange';
-        break;
+        case this.ST_PREPARATION:
+          this.allOff();
+          payBtn.style.display = 'none';
+          qrCode.style.display = 'none';
+          coin.style.display = 'none';
+          cup.style.display = 'block';
+          progressBar.style.width = '0%';
+          message.innerText = `Готовлю ${this.selectedDrink.name}`;
+          let progress = 0;
+          let interval = setInterval(() => {
+            progress += 1;
+            progressBar.style.width = `${progress}%`;
+            if (progress === 100) {
+              clearInterval(interval);
+              this.process(this.SIG_PREPARE);
+            }
+          }, 150);
+          break;
+  
+          case this.ST_FINISH:
+            this.allOff();
+            cup.style.display = 'block';
+            changeBtn.style.display = 'block';
+            message.innerText = 'Возьмите ваш напиток и не забудьте сдачу';
+            break;
+      }
+    }
+  
+    allOff() {
+      let payBtn = document.querySelector('.pay-btn');
+      let qrCode = document.querySelector('.qr-code');
+      let coin = document.querySelector('.coin');
+      let cup = document.querySelector('.cup');
+      let changeBtn = document.querySelector('.change-btn');
+      let returnedCoin = document.querySelector('.returned-coin');
+      payBtn.style.display = 'none';
+      qrCode.style.display = 'none';
+      coin.style.display = 'none';
+      cup.style.display = 'none';
+      changeBtn.setAttribute('disabled', 'disabled');
+      changeBtn.style.backgroundColor = '';
+      returnedCoin.style.display = 'none';
     }
   }
-
-  allOff() {
-    let payBtn = document.querySelector('.pay-btn');
-    let qrCode = document.querySelector('.qr-code');
-    let coin = document.querySelector('.coin');
-    let cup = document.querySelector('.cup');
-    let changeBtn = document.querySelector('.change-btn');
-    let returnedCoin = document.querySelector('.returned-coin');
-    payBtn.style.display = 'none';
-    qrCode.style.display = 'none';
-    coin.style.display = 'none';
-    cup.style.display = 'none';
-    changeBtn.setAttribute('disabled', 'disabled');
-    changeBtn.style.backgroundColor = '';
-    returnedCoin.style.display = 'none';      
-  }
-}
-
-let coffeeMachine = new CoffeeMachine();
-
-document.querySelectorAll('.drink-btn').forEach((btn) => {
-  btn.onclick = () => {
-    coffeeMachine.selectedDrink = {
-      name: btn.getAttribute('data-name'),
-      price: parseInt(btn.getAttribute('data-price')),
+  
+  let coffeeMachine = new CoffeeMachine();
+  
+  document.querySelectorAll('.drink-btn').forEach((btn) => {
+    btn.onclick = () => {
+      coffeeMachine.selectedDrink = {
+        name: btn.getAttribute('data-name'),
+        price: parseInt(btn.getAttribute('data-price')),
+      };
+      coffeeMachine.process(coffeeMachine.SIG_SELECT_DRINK);
     };
-    coffeeMachine.process(coffeeMachine.SIG_SELECT_DRINK);
+  });
+  
+  document.querySelector('.pay-btn').onclick = () => {
+    coffeeMachine.paidAmount += 100;
+    let paidAmountDisplay = document.querySelector('.paid-amount');
+    paidAmountDisplay.innerText = `Внесено: ${coffeeMachine.paidAmount} тнг`;
+  
+    let qrCode = document.querySelector('.qr-code');
+    qrCode.setAttribute('data-amount', coffeeMachine.paidAmount);
+  
+    if (coffeeMachine.paidAmount >= coffeeMachine.selectedDrink.price) {
+      coffeeMachine.process(coffeeMachine.SIG_PAY);
+    }
   };
-});
+  
+  document.querySelector('.coin').onclick = () => {
+    coffeeMachine.paidAmount += 100;
+    let paidAmountDisplay = document.querySelector('.paid-amount');
+    paidAmountDisplay.innerText = `Внесено: ${coffeeMachine.paidAmount} тнг`;
+  
+    if (coffeeMachine.paidAmount >= coffeeMachine.selectedDrink.price) {
+      coffeeMachine.process(coffeeMachine.SIG_PAY);
+    }
+  };
+  
+  document.querySelector('.cup').onclick = () => {
+    coffeeMachine.process(coffeeMachine.SIG_TAKE_DRINK);
+  };
+  
+  document.querySelector('.change-btn').onclick = () => {
+    coffeeMachine.process(coffeeMachine.SIG_RETURN_CHANGE_FINISH);
+    let returnedCoin = document.querySelector('.returned-coin');
+    returnedCoin.style.display = 'block';
+  };
 
-document.querySelector('.pay-btn').onclick = () => {
-  coffeeMachine.paidAmount += 100;
-  let paidAmountDisplay = document.querySelector('.paid-amount');
-  paidAmountDisplay.innerText = `Внесено: ${coffeeMachine.paidAmount} тнг`;
-
-  let qrCode = document.querySelector('.qr-code');
-  qrCode.setAttribute('data-amount', coffeeMachine.paidAmount);
-
-  if (coffeeMachine.paidAmount >= coffeeMachine.selectedDrink.price) {
-    coffeeMachine.process(coffeeMachine.SIG_PAY);
-  }
-};
-
-document.querySelector('.coin').onclick = () => {
-  coffeeMachine.paidAmount += 100;
-  let paidAmountDisplay = document.querySelector('.paid-amount');
-  paidAmountDisplay.innerText = `Внесено: ${coffeeMachine.paidAmount} тнг`;
-
-  if (coffeeMachine.paidAmount >= coffeeMachine.selectedDrink.price) {
-    coffeeMachine.process(coffeeMachine.SIG_PAY);
-  }
-};
-
-document.querySelector('.cup').onclick = () => {
-  coffeeMachine.process(coffeeMachine.SIG_TAKE_DRINK);
-};
-
-document.querySelector('.change-btn').onclick = () => {
-  coffeeMachine.process(coffeeMachine.SIG_RETURN_CHANGE);
-  let returnedCoin = document.querySelector('.returned-coin');
-  returnedCoin.style.display = 'block';
-};
-
-document.querySelector('.returned-coin').onclick = () => {
-  let returnedCoin = document.querySelector('.returned-coin');
-  returnedCoin.style.display = 'none';
-};
-
+  document.querySelector('.returned-coin').onclick = () => {
+    let returnedCoin = document.querySelector('.returned-coin');
+    returnedCoin.style.display = 'none';
+  };
+  
+  document.querySelector('.qr-code').onclick = () => {
+    coffeeMachine.paidAmount = coffeeMachine.selectedDrink.price;
+    let paidAmountDisplay = document.querySelector('.paid-amount');
+    paidAmountDisplay.innerText = `Внесено: ${coffeeMachine.paidAmount} тнг`;
+  
+    if (coffeeMachine.paidAmount >= coffeeMachine.selectedDrink.price) {
+      coffeeMachine.process(coffeeMachine.SIG_PAY);
+    }
+  };
