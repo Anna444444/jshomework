97c0c7e121ebdfa667c7c882a6165bf151a7a11e
Анна <124175366+Anna444444@users.noreply.github.com>
2023-05-16 20:21:41 +0600

    ДЗ38

--- ДЗ38/index.js
+++ ДЗ38/index.js
@@ -1,22 +1,25 @@
 const countryInput = document.getElementById('country-input');
 const cityInput = document.getElementById('city-input');
 const getWeatherButton = document.getElementById('get-weather');
-const currentWeather = document.querySelector('.current-weather');
-const forecast = document.querySelector('.forecast');
+const currentWeather = document.getElementById('current-weather-details');
+const forecast = document.getElementById('forecast-details');
 
-const geoApiKey = '6c993d61fd2a4e48aa4e0309383d880a';
+const countryList = document.getElementById('country-list');
+const cityList = document.getElementById('city-list');
+
+const geoApiKey = '7d2183b00c434181b6e7645c75f15233';
 const weatherApiKey = 'cf5b57c21ae743cea3f172828230305';
 
 countryInput.addEventListener('input', () => {
-    autocomplete(countryInput, 'country');
+    autocomplete(countryInput, 'country', countryList);
 });
 
 cityInput.addEventListener('input', () => {
     const countryCode = countryInput.getAttribute('data-country-code');
     if (countryCode) {
-        autocomplete(cityInput, 'city', countryCode);
+        autocomplete(cityInput, 'city', cityList, countryCode);
     } else {
-        autocomplete(cityInput, 'city');
+        autocomplete(cityInput, 'city', cityList);
     }
 });
 
@@ -24,77 +27,86 @@ getWeatherButton.addEventListener('click', () => {
     getWeatherData(cityInput.value);
 });
 
-function autocomplete(input, type, countryCode) {
+function autocomplete(input, type, listElement, countryCode) {
     const query = input.value;
     if (query.length < 2) return;
 
-    let url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&type=${type}&apikey=${geoApiKey}`;
+    let url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&apiKey=${geoApiKey}`;
 
     if (countryCode) {
         url += `&filter=countrycode:${countryCode}`;
     }
 
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            listElement.innerHTML = data.features.map(feat => {
+                return `<div data-value="${feat.properties.name}" data-country-code="${feat.properties.country_code}">${feat.properties.name}</div>`;
+            }).join('');
 
-            if (data.features.length > 0) {
-                const firstResult = data.features[0];
-                input.value = firstResult.properties.name;
-                if (type === 'country') {
-                    input.setAttribute('data-country-code', firstResult.properties.country_code);
-                }
-            }
-        }
-    };
-    xhr.open('GET', url, true);
-    xhr.send();
+            listElement.querySelectorAll('div').forEach(div => {
+                div.addEventListener('click', () => {
+                    input.value = div.getAttribute('data-value');
+                    listElement.innerHTML = '';
+                    if (type === 'country') {
+                        input.setAttribute('data-country-code', div.getAttribute('data-country-code'));
+                    }
+                });
+            });
+        });
 }
 
 function getWeatherData(city) {
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${city}&days=4`;
+    const currentWeatherUrl = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}&aqi=no`;
+    const forecastWeatherUrl = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${city}&days=3&aqi=no&alerts=no`;
 
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
-            const location = {
-                name: data.location.name,
+
+    fetch(currentWeatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            const location = {                name: data.location.name,
                 country: data.location.country
             };
 
             displayCurrentWeather(data.current, location);
-            displayForecast(data.forecast.forecastday.slice(1), location);
-        }
-    };
-    xhr.open('GET', url, true);
-    xhr.send();
+
+            fetch(forecastWeatherUrl)
+                .then(response => response.json())
+                .then(forecastData => {
+                    displayForecast(forecastData.forecast.forecastday, location);
+                });
+        });
 }
 
 function displayCurrentWeather(currentData, location) {
     const html = `
-        <h3>Текущая погода</h3>
-        <p>Температура: ${currentData.temp_c}°C</p>
-        <p>Ощущается как: ${currentData.feelslike_c}°C</p>
-        <p>Скорость ветра: ${currentData.wind_kph} км/ч</p>
-        <p>Направление ветра: ${currentData.wind_dir}</p>
-        <p>Влажность: ${currentData.humidity}%</p>
-        <p>Описание: ${currentData.condition.text}</p>
+    <h3>Текущая погода в ${location.name}, ${location.country}</h3>
+    <p>Температура: ${currentData.temp_c}°C</p>
+    <p>Ощущается как: ${currentData.feelslike_c}°C</p>
+    <p>Влажность: ${currentData.humidity}%</p>
+    <p>Скорость ветра: ${currentData.wind_kph} км/ч</p>
+    <p>Направление ветра: ${currentData.wind_dir}</p>
     `;
     currentWeather.innerHTML = html;
-    document.getElementById("location").innerText = `${location.name}, ${location.country}`;
 }
 
 function displayForecast(forecastData, location) {
-    const html = forecastData.map(day => `
-        <div>
-            <h4>${new Date(day.date).toLocaleDateString()}</h4>
-            <p>Макс: ${day.day.maxtemp_c}°C</p>
-            <p>Мин: ${day.day.mintemp_c}°C</p>
-            <p>Описание: ${day.day.condition.text}</p>
-        </div>
-    `).join('');
+    if (!forecastData) {
+        console.log('No forecast data received');
+        return;
+    }
+    const html = forecastData.map(day => {
+        return `
+            <div>
+                <h3>Прогноз на ${day.date} для ${location.name}, ${location.country}</h3>
+                <p>Макс. температура: ${day.day.maxtemp_c}°C</p>
+                <p>Мин. температура: ${day.day.mintemp_c}°C</p>
+                <p>Средняя скорость ветра: ${day.day.avgvis_km} км/ч</p>
+                <p>Макс. порыв ветра: ${day.day.maxwind_kph} км/ч</p>
+                <p>Вероятность осадков: ${day.day.daily_chance_of_rain}%</p>
+            </div>
+        `;
+    }).join('');
     forecast.innerHTML = html;
-    document.getElementById("location").innerText = `${location.name}, ${location.country}`;
 }
+
